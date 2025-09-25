a566b279ea3603e06bb3262513e1d5ac07cb4aee
Gabriel080103 <gabrielmarcussen@gmail.com>
2025-09-25 13:17:55 +0200

    sadfg

--- Task2/Main.java
+++ Task2/Main.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
 class Main{
 	
-	public void static main(){
 
 
+	public static void main(String [] args){
+
+		String input;
+		int age;
+		int deathTimer;
 
 	
+
+		Scanner scanner = new Scanner(System.in);
+
+			System.out.println("Please type your name: ");
+
+			 input = scanner.nextLine();
+
+		System.out.println("Hello " + input + ", please type your age");
+			
+
+			 age = scanner.nextInt();
+			 System.out.println("Congratiolations, you are " + age + " years old " + " How many siblings do you have?");
+
+
+			  deathTimer = scanner.nextInt();
+			 System.out.println("Congratiolations, you will die in " + deathTimer + " years.");
+
+
 	}
 
 
